d7e95a92f0c79fb432bb9131880de6f4ab9c184b
Rebecca Turner <rbt@sent.as>
2024-11-29 16:14:03 -0800

    Export `CXX` explicitly
    This fixes `./configure` on old versions of GHC on macOS

--- ghc.nix
+++ ghc.nix
@@ -262,6 +262,10 @@ hspkgs.shellFor rec {
   shellHook = ''
     # somehow, CC gets overridden so we set it again here.
     export CC=${stdenv.cc}/bin/cc
+    # This prevents `./configure` from detecting the system `g++` on macOS,
+    # fixing builds on some older GHC versions (like `ghc-9.7-start`):
+    #     /usr/bin/g++ -std=gnu++11: No such file or directory
+    export CXX=${stdenv.cc}/bin/c++
     export GHC=$NIX_GHC
     export GHCPKG=$NIX_GHCPKG
     export HAPPY=${happy}/bin/happy
